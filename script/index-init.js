926942a870dfcdc6a60f007694c8c4a34700ab4a
Adam Dzurilla <adamdzurilla19@gmail.com>
2023-10-19 22:28:09 +0200

    clean code

---
+++ script/index-init.js
@@ -0,0 +1,63 @@
+const CATEGORIES_FILE_PATH = "data/categories.json"
+
+class Category {
+    constructor(title, photoUrl, hrefContent) {
+        this.title = title;
+        this.photoUrl = photoUrl;
+        this.hrefContent = hrefContent;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+    var container = document.getElementById("categories");
+    let categories = await getCategories();
+    
+    categories.forEach(category => {
+        // Create a div element
+        var div = document.createElement("div");
+        div.classList.add("category-item")
+        div.addEventListener("click", function () {
+            window.location.href = category.hrefContent;
+        });
+
+        // Create an image element
+        var img = document.createElement("img");
+        img.src = category.photoUrl;
+        img.alt = category.title;
+
+        // Create a span element
+        var span = document.createElement("span");
+        span.textContent = category.title;
+
+        // Append the image and span to the div
+        div.appendChild(img);
+        div.appendChild(span);
+
+        // Append the div to the categories
+        container.appendChild(div);
+    })
+})
+
+async function getCategories() {
+    let result = [];
+    try {
+        const response = await fetch(CATEGORIES_FILE_PATH);
+        const data = await response.json()
+        for (const key in data) {
+            if (!data.hasOwnProperty(key)) {
+                continue;
+            }
+
+            const item = data[key];
+
+            let hrefContent = "models.html?model=" + key;
+            let category = new Category(item.title, item.photoUrl, hrefContent);
+
+            result.push(category);
+        }
+    } catch (error) {
+        console.error("Error fetching JSON data: ", error);
+    }
+
+    return result;
+}
