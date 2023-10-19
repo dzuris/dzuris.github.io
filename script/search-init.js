6aaa0161137e53a739b4d5832e44836373214d54
Adam Dzurilla <adamdzurilla19@gmail.com>
2023-10-19 22:28:24 +0200

    search item class

--- script/search-init.js
+++ script/search-init.js
@@ -1,13 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
     // Get the URL params
     const urlParams = new URLSearchParams(window.location.search);
 
     // Get the value
     const searchQuery = urlParams.get('searchQuery');
 
-    searchInData(searchQuery);
+    let data = await searchInData(searchQuery);
 });
 
-function searchInData(searchQuery) {
+async function searchInData(searchQuery) {
     console.log(searchQuery);
+}
+
+class searchItem {
+    constructor(categoryId, title, photoUrl, price, numOfKeywordsMatch, hrefToItem) {
+        this.categoryId = categoryId;
+        this.title = title;
+        this.photoUrl = photoUrl;
+        this.price = price;
+        this.numOfKeywordsMatch = numOfKeywordsMatch;
+        this.hrefToItem = hrefToItem;
+    }
 }
